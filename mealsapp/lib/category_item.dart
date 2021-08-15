ad4f111b9abc3f32b9a3eb6a2fe4fae3162ee71e
Hasan <qhasan966@gmail.com>
2021-08-16 02:36:00 +0500

    mealdecider

--- mealsapp/lib/category_item.dart
+++ mealsapp/lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mealsapp/category_meals_screen.dart';
 
 
 
@@ -8,18 +9,31 @@ class CategoryItem extends StatelessWidget {
 
   CategoryItem(this.title, this.color);
 
+  void selectCategory(BuildContext ctx)
+  {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_){
+      return CategoryMealsScreen();
+    }));
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title, style: Theme.of(context).textTheme.headline6,),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color.withOpacity(0.5), color], 
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: (){ selectCategory(context);},
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(title, style: Theme.of(context).textTheme.headline6,),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [color.withOpacity(0.5), color], 
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
     );
   }
