ade2e93c5a8e52480d8377044b245f53105b702e
Hasan <qhasan966@gmail.com>
2021-08-18 17:07:42 +0500

    mealdecider

--- mealsapp/lib/screens/meal_detail_screen.dart
+++ mealsapp/lib/screens/meal_detail_screen.dart
@@ -14,20 +14,17 @@ class MealDetailScreen extends StatelessWidget {
     );
   }
 
-  Widget buildContainer(var child)
-  {
+  Widget buildContainer(var child) {
     return Container(
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10)),
-              margin: EdgeInsets.all(10),
-              padding: EdgeInsets.all(10),
-              height: 150,
-              width: 300,
-              child: child
-    );
-  
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey),
+            borderRadius: BorderRadius.circular(10)),
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        height: 150,
+        width: 300,
+        child: child);
   }
 
   @override
@@ -50,8 +47,8 @@ class MealDetailScreen extends StatelessWidget {
                 ),
               ),
               buildSectionTitle(text: "Ingredients", context: context),
-              
-                buildContainer(ListView.builder(
+              buildContainer(
+                ListView.builder(
                   itemBuilder: (ctx, index) {
                     return Card(
                       color: Theme.of(context).accentColor,
@@ -65,15 +62,22 @@ class MealDetailScreen extends StatelessWidget {
                 ),
               ),
               buildSectionTitle(text: "Steps", context: context),
-              buildContainer(ListView.builder(itemBuilder: (ctx,index){
-                return Column(
-                  children: [
-                    ListTile(leading: CircleAvatar(child: Text("# ${index+1}"),),title: Text(selectMeal.steps[index]),),
-                    Divider()
-                  ],
-                );
-        
-              },itemCount: selectMeal.steps.length,))
+              buildContainer(ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          child: Text("# ${index + 1}"),
+                        ),
+                        title: Text(selectMeal.steps[index]),
+                      ),
+                      Divider()
+                    ],
+                  );
+                },
+                itemCount: selectMeal.steps.length,
+              ))
             ],
           ),
         ));
--- mealsapp/lib/screens/tabs_screen.dart
+++ mealsapp/lib/screens/tabs_screen.dart
@@ -10,28 +10,74 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  final List<Map<String,dynamic>> _pages = [
+    {
+      "page": CategoriesScreen(),
+      "title":"Categories"
+
+    },
+    {
+      "page":Favorites(),
+      "title":"Your Favorites"
+    }
+    ,
+    
+  ];
+  int _selectedpageindex=0;
+  void _selectpage(int index) {
+    setState(() {
+      _selectedpageindex=index;
+    });
+
+  }
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                  icon: Icon(
-                    Icons.star,
-                  ),
-                  text: "Favorites"),
-            ],
+    // return DefaultTabController(
+    //   length: 2,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text("Meals"),
+    //       bottom: TabBar(
+    //         tabs: [
+    //           Tab(
+    //             icon: Icon(Icons.category),
+    //             text: "Categories",
+    //           ),
+    //           Tab(
+    //               icon: Icon(
+    //                 Icons.star,
+    //               ),
+    //               text: "Favorites"),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(children: [CategoriesScreen(), Favorites()]),
+    //   ),
+    // );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedpageindex]['title']),
+      ),
+      body: _pages[_selectedpageindex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectpage,
+        items: [
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.category),
+            label: "Categories",
           ),
-        ),
-        body: TabBarView(children: [CategoriesScreen(), Favorites()]),
+          BottomNavigationBarItem(
+            // backgroundColor: Theme.of(context).primaryColor,
+            icon: Icon(Icons.star),
+            label: "Favorites",
+          ),
+        ],
+        backgroundColor: Theme.of(context).primaryColor,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        currentIndex: _selectedpageindex,
+        // type: BottomNavigationBarType.shifting,
       ),
     );
   }
